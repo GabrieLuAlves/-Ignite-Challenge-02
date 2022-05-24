3ea664b7b1b2aa61ba9a7b2e0d89bffdecc94a8a
Gabriel Luan Alves Valentim <gabrielluan.valentim@gmail.com>
2022-05-24 01:30:07 -0300

    Challenge submition #1

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,17 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.headers;
+
+    try {
+      return response.json(
+        this.listAllUsersUseCase.execute({
+          user_id: typeof user_id === "string" ? user_id : user_id[0],
+        })
+      );
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
   }
 }
 
--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,19 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error(
+        "The informed user id does not belong to any existing user."
+      );
+    }
+
+    if (!user.admin) {
+      throw new Error("The informed user id corresponds to a non-admin user.");
+    }
+
+    return this.usersRepository.list();
   }
 }
 
